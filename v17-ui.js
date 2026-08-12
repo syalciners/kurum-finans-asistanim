4ffe6e899a964af81860815746444f326f9f259a
syalciners <syalciners@gmail.com>
2026-08-12 11:16:04 +0300

    V1.7.5 UX modülünü yükle

--- v17-ui.js
+++ v17-ui.js
@@ -613,4 +613,12 @@
   document.querySelector('#incomeOwnerKpis')?.addEventListener('click', () => setTimeout(updateMainKpiState, 0));
 
   updateMainKpiState();
+
+  // V1.7.5 modülünü mevcut V1.7 katmanından yükle.
+  if(!document.querySelector('script[data-v175-loader]')){
+    const script = document.createElement('script');
+    script.src = 'v175-ui.js?v=175';
+    script.dataset.v175Loader = '1';
+    document.head.appendChild(script);
+  }
 })();
