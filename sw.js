7d734ad35fa87f2770250e6f801bc42b117977f1
syalciners <syalciners@gmail.com>
2026-08-15 07:57:32 +0300

    V250: Ödemeler polish dosyasını PWA paketine ekle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bs-ofis-butce-ui249-debts-polish-20260815';
+const CACHE='bs-ofis-butce-ui250-payments-polish-20260815';
 const ASSETS=[
   './?v=240',
   './index.html?v=240',
@@ -7,6 +7,7 @@ const ASSETS=[
   './v2471-mobile-polish.css?v=2471',
   './v248-dashboard-polish.css?v=248',
   './v249-debts-polish.css?v=249',
+  './v250-payments-polish.css?v=250',
   './app.js?v=176',
   './core-compat.js?v=240',
   './v17-ui.js?v=218',
