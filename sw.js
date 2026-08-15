565ff8db2a1c44dd4ebafcf8aa42773c35aa98ad
syalciners <syalciners@gmail.com>
2026-08-15 07:59:24 +0300

    V251: Gelirler polish dosyasını PWA paketine ekle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bs-ofis-butce-ui250-payments-polish-20260815';
+const CACHE='bs-ofis-butce-ui251-incomes-polish-20260815';
 const ASSETS=[
   './?v=240',
   './index.html?v=240',
@@ -8,6 +8,7 @@ const ASSETS=[
   './v248-dashboard-polish.css?v=248',
   './v249-debts-polish.css?v=249',
   './v250-payments-polish.css?v=250',
+  './v251-incomes-polish.css?v=251',
   './app.js?v=176',
   './core-compat.js?v=240',
   './v17-ui.js?v=218',
